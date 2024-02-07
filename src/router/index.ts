2b6483fbce9295cb9bd7660fd471e3afb1252dcc
somayerohani6 <somayerohani6@gmail.com>
2024-02-07 15:58:20 +0300

    re-write

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -21,3 +22,34 @@ const router = createRouter({
 })
 
 export default router
+=======
+import { createRouter, createWebHashHistory } from 'vue-router'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewEditNote from '@/views/ViewEditNote.vue'
+import ViewStats from '@/views/ViewStats.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'notes',
+    component: ViewNotes
+  },
+  {
+    path: '/editNote/:id',
+    name: 'edit-note',
+    component: ViewEditNote
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: ViewStats
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
+>>>>>>> a00b285 (re-write)
